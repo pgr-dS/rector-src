ace69eaecfe8829b21b24d90d5e4c2e9f23f2521
GitHub Action <actions@github.com>
2023-04-19 08:59:28 +0000

    [ci-review] Rector Rectify

--- rules-tests/TypeDeclaration/Rector/BooleanAnd/BinaryOpNullableToInstanceofRector/config/configured_rule.php
+++ rules-tests/TypeDeclaration/Rector/BooleanAnd/BinaryOpNullableToInstanceofRector/config/configured_rule.php
@@ -1,9 +1,10 @@
 <?php
 
 declare(strict_types=1);
+use Rector\TypeDeclaration\Rector\BooleanAnd\BinaryOpNullableToInstanceofRector;
 
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(\Rector\TypeDeclaration\Rector\BooleanAnd\BinaryOpNullableToInstanceofRector::class);
+    $rectorConfig->rule(BinaryOpNullableToInstanceofRector::class);
 };
